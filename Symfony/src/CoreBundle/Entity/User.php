f472f3b063df6888b4cf880a8403d33f61b57bc8
Leo Grambert <leogrambert@gmail.com>
2017-06-27 11:41:46 +0200

    I've started to work on HWIOAuthBundle.

--- Symfony/src/CoreBundle/Entity/User.php
+++ Symfony/src/CoreBundle/Entity/User.php
@@ -22,18 +22,66 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $facebookID;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $googleID;
+
     public function __construct()
     {
         parent::__construct();
     }
 
-    public function setEnabled($boolean)
+    /**
+     * Set facebookID
+     *
+     * @param string $facebookID
+     *
+     * @return User
+     */
+    public function setFacebookID($facebookID)
     {
-        if ($this->getRoles() == ['ROLE_PRO'] ){
-            $this->enabled = false;
-        } else {
-            $this->enabled = true;
-        }
-        return $this->enabled;
+        $this->facebookID = $facebookID;
+
+        return $this;
+    }
+
+    /**
+     * Get facebookID
+     *
+     * @return string
+     */
+    public function getFacebookID()
+    {
+        return $this->facebookID;
+    }
+
+    /**
+     * Set googleID
+     *
+     * @param string $googleID
+     *
+     * @return User
+     */
+    public function setGoogleID($googleID)
+    {
+        $this->googleID = $googleID;
+
+        return $this;
+    }
+
+    /**
+     * Get googleID
+     *
+     * @return string
+     */
+    public function getGoogleID()
+    {
+        return $this->googleID;
     }
 }
